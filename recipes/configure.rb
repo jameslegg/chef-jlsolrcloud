8a20c372d4a8af7547e47c47f6615b1180e2fbe1
James Legg <james.legg@conversocial.com>
2015-03-26 16:36:45 +0000

    solrcloud solr.xml solr_home overide and solrcloud enable

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -4,6 +4,16 @@ if node['jlsolrcloud']['zkhosts'].length < 1
   fail "At least one zkhost is needed in node['jlsolrcloud']['zkhosts']"
 end
 
+if node['jlsolrcloud']['solr_home_override']
+  cookbook_file "#{node['jlsolrcloud']['solr_home_override']}/solr.xml" do
+    source 'solr.xml'
+    user   node['jlsolrcloud']['user']
+    group  node['jlsolrcloud']['group']
+    mode   0755
+    notifies :restart, 'service[solr]'
+  end
+end
+
 template "#{node['jlsolrcloud']['solr_home']}/solr.in.sh" do
   user  node['jlsolrcloud']['user']
   group node['jlsolrcloud']['group']
