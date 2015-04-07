01d3710ef7cbc8debe59f9b1cc93189b0ce33d6e
James Legg <james.legg@conversocial.com>
2015-04-07 17:16:01 +0100

    use copytruncate, and variable for paths

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### 0.2.1
+ - copytruncate the log file
+
 ### 0.2.0
  - Add Options to manage rotation of Garbage Collection Logs
  - Rotate the log from solr's standard out.
--- recipes/_logrotate.rb
+++ recipes/_logrotate.rb
@@ -1,7 +1,8 @@
 include_recipe 'logrotate'
 
 logrotate_app 'solr' do
-  path '/var/solr/log/solr-8983-console.log'
+  path "#{node['jlsolrcloud']['solr_home']}/logs/solr-8983-console.log"
+  cookbook 'jlsolrcloud'
   frequency 'daily'
   rotate 3
   only_if node['jlsolrcloud']['log_rotate']
