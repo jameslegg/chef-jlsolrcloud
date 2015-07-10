87166aa0fb008d9cdba2f3603655b4addeeb29e7
James Legg <james.legg@conversocial.com>
2015-07-10 10:49:53 +0100

    better logging

--- recipes/_logrotate.rb
+++ recipes/_logrotate.rb
@@ -1,9 +1,10 @@
 include_recipe 'logrotate'
 
+# This configure log rotation for the default solr console log ONLY
 logrotate_app 'solr' do
   path "#{node['jlsolrcloud']['solr_home']}/logs/solr-8983-console.log"
   cookbook 'jlsolrcloud'
-  frequency 'daily'
-  rotate 3
+  frequency node['jlsolrcloud']['log_rotate']['frequency']
+  rotate node['jlsolrcloud']['log_rotate']['rotate']
   only_if node['jlsolrcloud']['log_rotate']
 end
