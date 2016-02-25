410e52c1a2584bbec474ce89d56db487bb354ea6
Aaron Kalair <aaronkalair@gmail.com>
2016-02-25 09:24:45 +0000

    feat: noticket - Add switch to skip installing Solr if its installed elsewhere

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,5 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-include_recipe 'jlsolrcloud::install'
+#
+# We have an AMI that preinstalls Solr so want to skip this to save time
+if node['jlsolrcloud']['install_solr']
+  include_recipe 'jlsolrcloud::install'
+end
 include_recipe 'jlsolrcloud::configure'
